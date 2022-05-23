635c076513e48ccfd7a5a32c4ebdb1a40ab8a866
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-05-23 22:19:30 +0530

    adding alter.cpp

---
+++ alter.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vip vector<pair<int, int>>
+#define endl "\n"
+
+
+void solve()
+{
+   int a,b,p,q;
+   cin>>a>>b>>p>>q;
+
+   if(p % a != 0 || q % b != 0){
+       cout<<"no"<<endl;
+   }
+   
+   int n1 = p / a;
+   int n2 = q / b;
+
+   if(-1 <= n1-n2 || n1-n2 <= 1){
+        cout<<"yes"<<endl;
+   }
+   else{
+       cout<<"no"<<endl;
+   }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
